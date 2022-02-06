f46be8e93c4571356640130d793bb7338a8ebc9d
bonzali <katbonzali@gmail.com>
2022-02-05 16:27:43 -0800

    update home page : add title to photoList

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import './style.scss'
-import UserList from "components/UserList";
-import PhotoList from 'components/PhotoList'
-import UserFeed from "components/UserFeed";
-import {FaEdit , FaCross} from "react-icons/fa";
-import {User} from "definitions/user";
-import CreatePost from "components/CreatePost"
+import './style.scss';
+import UserList from 'components/UserList';
+import PhotoList from 'components/PhotoList';
+import UserFeed from 'components/UserFeed';
+import { FaEdit, FaCross } from 'react-icons/fa';
+import { User } from 'definitions/user';
+import CreatePost from 'components/CreatePost';
 type Props = {
-    logout : Function
-}
-function Home({logout} : Props) {
-    const [user , setUser] = React.useState<User | undefined>()
-    const [isOpen , setIsOpen] = React.useState(false)
-    return (
-        <div className={'Home'}>
-                <button onClick={()=> setIsOpen(true)} className={'new-post-button'}> <FaEdit/></button>
-               <UserList onUserSelected={(user) => setUser(user)}/>
-               {user ? <UserFeed logout={logout} user={user}/> : <PhotoList/>}
-               <div className={`sidebar ${isOpen ? 'active' : ''}`}>
-                   <CreatePost onClose={() => setIsOpen(false)}/>
-               </div>
+  logout: Function;
+};
+function Home({ logout }: Props) {
+  const [user, setUser] = React.useState<User | undefined>();
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <div className={'Home'}>
+      <button onClick={() => setIsOpen(true)} className={'new-post-button'}>
+        {' '}
+        <FaEdit />
+      </button>
+      <UserList onUserSelected={(user) => setUser(user)} />
+      {user ? (
+        <UserFeed logout={logout} user={user} />
+      ) : (
+        <div className='photo-container'>
+          <h3>Community</h3>
+          <PhotoList />
         </div>
-    );
+      )}
+      <div className={`sidebar ${isOpen ? 'active' : ''}`}>
+        <CreatePost onClose={() => setIsOpen(false)} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/PhotoList/PhotoList.tsx
+++ src/components/PhotoList/PhotoList.tsx
@@ -1,46 +1,51 @@
-import React, {useEffect, useState} from 'react';
-import './PhotoList.scss'
-import {getPhotoList} from "api/photo"
-import PhotoListItem from "./PhotoListItem"
-import StackGrid from "shared/components/StackGrid";
-import {useWindowSize} from "shared/hooks/useWindowSize";
-import {Photo} from "definitions/photo";
-import Loader from "shared/components/Loader";
+import React, { useEffect, useState } from 'react';
+import './PhotoList.scss';
+import { getPhotoList } from 'api/photo';
+import PhotoListItem from './PhotoListItem';
+import StackGrid from 'shared/components/StackGrid';
+import { useWindowSize } from 'shared/hooks/useWindowSize';
+import { Photo } from 'definitions/photo';
+import Loader from 'shared/components/Loader';
 
 type Props = {
-    userId ?: number
-}
+  userId?: number;
+};
 
-function PhotoList({userId} : Props) {
-    const {width} = useWindowSize()
-    const [isLoading, setIsLoading] = useState(true)
-    const [photos, setPhotos] = useState<Photo[]>([])
+function PhotoList({ userId }: Props) {
+  const { width } = useWindowSize();
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [photos, setPhotos] = useState<Photo[]>([]);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const result = await getPhotoList(userId)
-                setPhotos(result)
-                setIsLoading(false)
-            } catch (e) {
-                //Todo : display error message when request failed
-                setIsLoading(false)
-            }        })()
-    }, [userId])
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getPhotoList(userId);
+        setPhotos(result);
+        setIsLoading(false);
+      } catch (e) {
+        setError('Sorry!! Something went wrong');
+        setIsLoading(false);
+      }
+    })();
+  }, [userId]);
 
-    return (
-        <div className={'PhotoList'}>
-            {isLoading && <Loader/>}
-            <StackGrid style={{marginTop: '38px'}}
-                       columnWidth={width <= 768 ? '49%' : '19%'}
-            >
-                {
-                    photos.map(photo => (<PhotoListItem key={photo.id} photo={photo}/>))
-                }
-
-            </StackGrid>
-        </div>
-    );
+  return (
+    <div className={'PhotoList'}>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <StackGrid
+          style={{ marginTop: '38px' }}
+          columnWidth={width <= 768 ? '49%' : '19%'}
+        >
+          {photos.map((photo) => (
+            <PhotoListItem key={photo.id} photo={photo} />
+          ))}
+        </StackGrid>
+      )}
+    </div>
+  );
 }
 
 export default PhotoList;
